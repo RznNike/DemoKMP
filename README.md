a183190b42ed39c2ff63a4eb3c1e3d131c495b16
RznNike <rznnike@yandex.ru>
2024-07-26 14:33:12 +0300

    Project init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Demo Kotlin Multiplatform project targeting Desktop
+Demo Kotlin Multiplatform project targeting Desktop.
+
+To launch project from IDE:
+
+New run configuration -> Gradle -> add args
+```desktopRun -DmainClass=MainKt --quiet```
---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+kotlin {
+    jvm("desktop")
+    
+    sourceSets {
+        val desktopMain by getting
+        
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.serialization.json)
+        }
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "ru.rznnike.demokmp"
+            packageVersion = "1.0.0"
+        }
+    }
+}
