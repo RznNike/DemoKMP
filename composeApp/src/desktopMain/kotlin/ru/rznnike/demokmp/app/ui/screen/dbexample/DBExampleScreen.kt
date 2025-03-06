2570621cad19338ef02431a2a82e9b0e87199f47
RznNike <rznnike@yandex.ru>
2025-03-06 15:13:42 +0300

    DBExampleScreen redesigned

--- composeApp/src/desktopMain/kotlin/ru/rznnike/demokmp/app/ui/screen/dbexample/DBExampleScreen.kt
+++ composeApp/src/desktopMain/kotlin/ru/rznnike/demokmp/app/ui/screen/dbexample/DBExampleScreen.kt
@@ -1,18 +1,15 @@
 package ru.rznnike.demokmp.app.ui.screen.dbexample
 
 import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -20,10 +17,7 @@ import org.jetbrains.compose.resources.stringResource
 import ru.rznnike.demokmp.app.navigation.NavigationScreen
 import ru.rznnike.demokmp.app.navigation.getNavigator
 import ru.rznnike.demokmp.app.ui.item.DBExampleDataItem
-import ru.rznnike.demokmp.app.ui.theme.LocalCustomColorScheme
-import ru.rznnike.demokmp.app.ui.view.TextR
-import ru.rznnike.demokmp.app.ui.view.Toolbar
-import ru.rznnike.demokmp.app.ui.view.ToolbarButton
+import ru.rznnike.demokmp.app.ui.view.*
 import ru.rznnike.demokmp.app.utils.onEnterKey
 import ru.rznnike.demokmp.app.viewmodel.dbexample.DBExampleViewModel
 import ru.rznnike.demokmp.generated.resources.*
@@ -46,15 +40,16 @@ class DBExampleScreen : NavigationScreen() {
 
         var showToolbarMenu by remember { mutableStateOf(false) }
 
-        Column {
-            Spacer(Modifier.height(16.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
             Box(
                 contentAlignment = Alignment.TopEnd
             ) {
                 Toolbar(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     title = stringResource(Res.string.db_example),
                     leftButton = ToolbarButton(Res.drawable.ic_back) {
                         navigator.closeScreen()
@@ -63,12 +58,11 @@ class DBExampleScreen : NavigationScreen() {
                         showToolbarMenu = true
                     }
                 )
-                Box(
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                ) {
+                Box {
                     DropdownMenu(
                         expanded = showToolbarMenu,
-                        onDismissRequest = { showToolbarMenu = false }
+                        onDismissRequest = { showToolbarMenu = false },
+                        containerColor = MaterialTheme.colorScheme.surface
                     ) {
                         DropdownMenuItem(
                             text = {
@@ -85,72 +79,59 @@ class DBExampleScreen : NavigationScreen() {
             Spacer(Modifier.height(16.dp))
 
             Surface(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 shape = MaterialTheme.shapes.medium,
-                color = MaterialTheme.colorScheme.surfaceContainerHigh
+                color = MaterialTheme.colorScheme.surface
             ) {
                 Row(
-                    modifier = Modifier
-                        .padding(
-                            start = 16.dp,
-                            end = 16.dp,
-                            top = 8.dp,
-                            bottom = 16.dp
-                        )
-                        .fillMaxWidth()
-                        .height(IntrinsicSize.Min)
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    OutlinedTextField(
+                    Spacer(Modifier.width(16.dp))
+                    SlimOutlinedTextField(
+                        modifier = Modifier
+                            .padding(top = 8.dp, bottom = 16.dp)
+                            .weight(1f)
+                            .onEnterKey {
+                                viewModel.addData()
+                            },
                         value = viewModel.nameInput,
                         singleLine = true,
                         label = {
                             TextR(Res.string.db_example_input_label)
                         },
-                        modifier = Modifier
-                            .weight(1f)
-                            .onEnterKey {
-                                viewModel.addData()
-                            },
                         onValueChange = viewModel::onNameInput
                     )
-                    Spacer(Modifier.width(16.dp))
-                    Button(
-                        modifier = Modifier
-                            .padding(top = 8.dp)
-                            .fillMaxHeight(),
+                    Spacer(Modifier.width(12.dp))
+                    SelectableButton(
+                        modifier = Modifier.padding(vertical = 12.dp),
                         onClick = {
                             viewModel.addData()
                         }
                     ) {
-                        TextR(Res.string.add)
+                        TextR(Res.string.send)
                     }
+                    Spacer(Modifier.width(12.dp))
                 }
             }
 
             Spacer(Modifier.height(16.dp))
-            Box(
+            Surface(
                 modifier = Modifier
-                    .fillMaxSize()
                     .weight(1f)
+                    .fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium,
+                color = MaterialTheme.colorScheme.surface
             ) {
-                val state = rememberLazyListState()
                 Box(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .fillMaxSize()
-                        .clip(
-                            MaterialTheme.shapes.medium.copy(
-                                bottomStart = CornerSize(0.dp),
-                                bottomEnd = CornerSize(0.dp)
-                            )
-                        )
+                    modifier = Modifier.fillMaxSize()
                 ) {
+                    val state = rememberLazyListState()
                     LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp),
                         state = state,
-                        modifier = Modifier.fillMaxSize(),
-                        contentPadding = PaddingValues(bottom = 16.dp),
+                        contentPadding = PaddingValues(vertical = 16.dp),
                         verticalArrangement = Arrangement.spacedBy(16.dp)
                     ) {
                         items(
@@ -162,26 +143,21 @@ class DBExampleScreen : NavigationScreen() {
                             }
                         }
                     }
+                    VerticalScrollbar(
+                        modifier = Modifier
+                            .align(Alignment.CenterEnd)
+                            .fillMaxHeight(),
+                        adapter = rememberScrollbarAdapter(state)
+                    )
 
                     if (uiState.isLoading) {
-                        Box(
+                        CircularProgressIndicator(
                             modifier = Modifier
-                                .fillMaxSize()
-                                .background(LocalCustomColorScheme.current.surfaceContainerA50),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            CircularProgressIndicator(
-                                modifier = Modifier.size(50.dp)
-                            )
-                        }
+                                .size(48.dp)
+                                .align(Alignment.Center)
+                        )
                     }
                 }
-                VerticalScrollbar(
-                    modifier = Modifier
-                        .align(Alignment.CenterEnd)
-                        .fillMaxHeight(),
-                    adapter = rememberScrollbarAdapter(state)
-                )
             }
         }
     }
