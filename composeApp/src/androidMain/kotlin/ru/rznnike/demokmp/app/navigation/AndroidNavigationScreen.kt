ec13578ff5fd2ae6c2dfb32ffaa41616814a1e84
RznNike <rznnike@yandex.ru>
2025-03-26 15:57:56 +0300

    Back button custom handling added

--- composeApp/src/androidMain/kotlin/ru/rznnike/demokmp/app/navigation/AndroidNavigationScreen.kt
+++ composeApp/src/androidMain/kotlin/ru/rznnike/demokmp/app/navigation/AndroidNavigationScreen.kt
@@ -1,6 +1,7 @@
 package ru.rznnike.demokmp.app.navigation
 
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,13 +15,20 @@ abstract class AndroidNavigationScreen : NavigationScreen() {
     open val isLightNavigationBar: Boolean
         @Composable
         get() = !isSystemInDarkTheme()
+
     private val activity: ComponentActivity?
         @Composable
         get() = LocalContext.current as ComponentActivity?
 
+    @Transient
+    var onBackPressedCallback: (navigator: FlowNavigator) -> Unit = { navigator ->
+        navigator.closeScreen()
+    }
+
     @Composable
     final override fun Content() {
         setSystemBarsColors()
+        handleBackPress()
         super.Content()
     }
 
@@ -35,6 +43,14 @@ abstract class AndroidNavigationScreen : NavigationScreen() {
                     isAppearanceLightNavigationBars = isLightNB
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun handleBackPress() {
+        val navigator = getNavigator()
+        BackHandler {
+            onBackPressedCallback(navigator)
         }
     }
 }
