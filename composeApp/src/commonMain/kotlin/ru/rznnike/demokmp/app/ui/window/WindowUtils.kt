752bfb34586c8673bc22cb11d899dafbd51e5da5
RznNike <rznnike@yandex.ru>
2025-03-31 17:19:46 +0300

    Specific stuff moved to desktop module

--- composeApp/src/commonMain/kotlin/ru/rznnike/demokmp/app/ui/window/WindowUtils.kt
+++ composeApp/src/commonMain/kotlin/ru/rznnike/demokmp/app/ui/window/WindowUtils.kt
@@ -1,7 +1,5 @@
 package ru.rznnike.demokmp.app.ui.window
 
 import androidx.compose.runtime.staticCompositionLocalOf
-import ru.rznnike.demokmp.app.dispatcher.keyboard.KeyEventDispatcher
 
-val LocalKeyEventDispatcher = staticCompositionLocalOf { KeyEventDispatcher(null) }
 val LocalWindowCloseCallback = staticCompositionLocalOf { { } }
