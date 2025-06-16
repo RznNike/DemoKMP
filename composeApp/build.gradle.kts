2601fcb23e540ed9951689e2a08da7558cc6b1b6
RznNike <rznnike@yandex.ru>
2025-06-16 12:31:16 +0300

    Default Esc handling disabled

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.ui.backhandler)
 
             implementation(libs.viewmodel.compose)
 
--- composeApp/src/commonMain/kotlin/ru/rznnike/demokmp/app/navigation/NavigationUtils.kt
+++ composeApp/src/commonMain/kotlin/ru/rznnike/demokmp/app/navigation/NavigationUtils.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.backhandler.BackHandler
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -14,6 +16,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import ru.rznnike.demokmp.app.utils.windowViewModel
 import ru.rznnike.demokmp.app.viewmodel.global.configuration.WindowConfigurationViewModel
+import ru.rznnike.demokmp.domain.utils.OperatingSystem
 import kotlin.reflect.KType
 
 private const val SCREEN_ANIMATION_DURATION_MS = 500
@@ -21,6 +24,7 @@ private const val SCREEN_ANIMATION_DURATION_MS = 500
 val LocalNavController = staticCompositionLocalOf { NavController() }
 val LocalNavigationStructure = staticCompositionLocalOf { mutableListOf<Int>() }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun createNavHost(flow: NavigationFlow) {
     val navController = rememberNavController()
@@ -36,6 +40,9 @@ fun createNavHost(flow: NavigationFlow) {
             exitTransition = { fadeOut(animationSpec = tween(SCREEN_ANIMATION_DURATION_MS)) }
         ) {
             buildNavGraph()
+        }
+        if (OperatingSystem.isDesktop) {
+            BackHandler {} // Disable default Esc handling in favor of custom hotkeys listener
         }
 
         val navigator = getNavigator()
