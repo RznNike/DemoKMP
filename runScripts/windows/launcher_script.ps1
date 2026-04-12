a0173445d261f0fced97b89f9b23e69b885881aa
RznNike <rznnike@yandex.ru>
2026-04-12 23:45:44 +0300

    More direct java version detection in windows launcher script

--- runScripts/windows/launcher_script.ps1
+++ runScripts/windows/launcher_script.ps1
@@ -17,7 +17,14 @@ if ([System.IO.File]::Exists($configuration_path)) {
     $configuration = Get-Content $configuration_path -Raw | ConvertFrom-StringData
 
     $java_command = if ([string]$configuration.java_path -ne "") { $configuration.java_path } else { "java" }
-    $java_version = (Get-Command $java_command | Select-Object -ExpandProperty Version).Major
+    $version_output = & $java_command -version 2>&1
+        $version_string = ($version_output | Select-String "version").ToString()
+        $java_full_version = [regex]::Match($version_string, '\"(.*?)\"').Groups[1].Value
+        if ($java_full_version.StartsWith("1.")) {
+            $java_version = $java_full_version.Split('.')[1] # v1-8
+        } else {
+            $java_version = $java_full_version.Split('.')[0] # v9+
+        }
     if ([int]$java_version -lt 17) {
         $displayed_version = if ([string]$java_version -ne "") { $java_version } else { "none" }
         ShowErrorMessage("Java 17+ required for launch, current version - $($displayed_version)")
